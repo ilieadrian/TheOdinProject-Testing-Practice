d3de31625a9bd9277871fa8b1ab0b5f5e3fc2bae
I A <ilie.adrian1986@gmail.com>
2025-03-24 16:21:11 +0200

    added basic test and linking between file, instaled jest

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("Hello world")
+function sum(a, b) {
+    return a + b;
+}
+module.exports = sum;
--- script.test.js
+++ script.test.js
@@ -1,1 +1,5 @@
-console.log("Hello world")
+const sum = require('./script');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
